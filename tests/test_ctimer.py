b3780bb2fb84817bea264f2126d0c1c16031347c
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2020-06-10 22:50:34 +0800

    test(metadata): add meta object test cases

--- tests/test_ctimer.py
+++ tests/test_ctimer.py
@@ -9,16 +9,53 @@ from concentratetimer import concentratetimer
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
+def metadata_class():
+    return concentratetimer.Meta
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+
+def test_metadata_default(metadata_class):
+    """long_break_clock_count default value"""
+    metadata_default = metadata_class()
+
+    assert metadata_default.long_break_clock_count == 4
+
+
+def test_metadata_long_break_clock_count_1(metadata_class):
+    """assigned long_break_clock_count value with integer less than 2"""
+    metadata_default = metadata_class(long_break_clock_count=1)
+
+    assert metadata_default.long_break_clock_count == 2
+
+
+def test_metadata_long_break_clock_count_2(metadata_class):
+    """assigned long_break_clock_count value with non-integer less than 2"""
+    metadata_default = metadata_class(long_break_clock_count=1.5)
+
+    assert metadata_default.long_break_clock_count == 2
+
+
+def test_metadata_long_break_clock_count_3(metadata_class):
+    """assigned long_break_clock_count value with integer larger than 2"""
+    metadata_default = metadata_class(long_break_clock_count=3)
+
+    assert metadata_default.long_break_clock_count == 3
+
+
+def test_metadata_long_break_clock_count_4(metadata_class):
+    """assigned long_break_clock_count value with non-integer larger than 2
+
+    Besides, testing downward rounding
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    metadata_default = metadata_class(long_break_clock_count=4.4)
+
+    assert metadata_default.long_break_clock_count == 4
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_metadata_long_break_clock_count_5(metadata_class):
+    """assigned long_break_clock_count value with non-integer larger than 2
+
+    Besides, testing upward rounding
+    """
+    metadata_default = metadata_class(long_break_clock_count=5.5)
+
+    assert metadata_default.long_break_clock_count == 6
