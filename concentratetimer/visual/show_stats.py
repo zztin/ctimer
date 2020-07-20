9012b0fd584f4fd30e44fdeefa8cb07debdceb49
zztin <32836585+zztin@users.noreply.github.com>
2020-07-20 09:05:47 +0200

    show stats draft

---
+++ concentratetimer/visual/show_stats.py
@@ -0,0 +1,21 @@
+# https://plotly.com/python/gantt/
+
+# Gantt
+# import plotly.figure_factory as ff
+#
+# df = [dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28'),
+#       dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15'),
+#       dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30')]
+#
+# fig = ff.create_gantt(df)
+# fig.show()
+
+
+#  Time schedule:
+# With floating inlet text about what you did in that clock
+# https://stackoverflow.com/questions/20892411/create-a-weekly-timetable-using-matplotlib
+# Bokeh patches
+# https://docs.bokeh.org/en/latest/docs/user_guide/plotting.html
+# Clock count
+#https://community.plotly.com/t/colored-calendar-heatmap-in-dash/10907/9
+#https://docs.bokeh.org/en/latest/docs/gallery/range_tool.html
