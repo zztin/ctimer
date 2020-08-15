2c2bdf47bf5286af63d09a4ae4591ec5983f36b1
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2020-08-15 13:06:17 +0800

    feat(db): add an option to customize db path
    BREAKING CHANGE: issue: #26

--- concentratetimer/cli.py
+++ concentratetimer/cli.py
@@ -5,7 +5,24 @@ import concentratetimer.ctimer_db as db
 import sys
 import os
 import argparse
+import logging
 from concentratetimer.visual import clock_count_cal as ccc
+
+
+def dir_path(rdir_path):
+    root_path = os.path.dirname(concentratetimer.__file__).rsplit("/",1)[0]
+    full_path = os.path.join(root_path, rdir_path)
+
+    if os.path.exists(full_path):
+        logging.info(f"Found existing db path {full_path}. Will re-use it.")
+    else:
+        logging.info(f"db path {full_path}/data not found. Create an new one.")
+        os.makedirs(f"{full_path}")
+
+    if os.path.isdir(full_path):
+        return full_path
+    else:
+        raise NotADirectoryError(full_path)
 
 
 def main():
@@ -15,19 +32,19 @@ def main():
     parser.add_argument("--hide", help="Display the timer always on top of other windows unless this statement is given"
                         , action="store_true")
     parser.add_argument("--silence", help="Silence Mode (visual hint instead of audio hint.", action="store_true")
+    parser.add_argument("--db", type=dir_path, help="The relative or absolute folder path to store and/or read db",
+                        default="./data")
     args = parser.parse_args()
+
     # can also prompt user to enter a file path to store the database, but next time when the program launch
     #  it has to find it automatically. "from tkinter import filedialog"
-    path = os.path.dirname(concentratetimer.__file__).rsplit("/",1)[0]
-    if os.path.exists(f"{path}/data"):
-        pass
-    else:
-        os.makedirs(f"{path}/data/")
+    logging.info(f"{args.db} is where the db stored in.")
+
     if args.debug:
-        db_file = f"{path}/data/ctimer_debug.db"
+        db_file = f"{args.db}/ctimer_debug.db"
         db.create_connection(db_file) # create if not exist
     else:
-        db_file = f"{path}/data/ctimer.db"
+        db_file = f"{args.db}/ctimer.db"
         db.create_connection(db_file) # create if not exist
     if args.overall:
         events = db.get_yearly_stats(db_file)
