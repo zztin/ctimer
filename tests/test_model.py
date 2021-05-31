bf27450cf6c025ed88269d5552db09e6e8e5042f
Liting <liting@Litings-MacBook-Air.local>
2021-05-31 19:10:28 +0200

    include more tests.

--- tests/test_model.py
+++ tests/test_model.py
@@ -25,37 +25,37 @@ def metadata_class():
 
 
 def test_metadata_default(metadata_class):
-    """long_break_clock_count default value"""
+    """Test long_break_clock_count default value."""
     metadata_default = metadata_class()
 
     assert metadata_default.long_break_clock_count == 4
 
 
 def test_metadata_long_break_clock_count_1(metadata_class):
-    """assigned long_break_clock_count value with integer less than 2"""
+    """Test long_break_clock_count <2 (int) round up to 2"""
     metadata_default = metadata_class(long_break_clock_count=1)
 
     assert metadata_default.long_break_clock_count == 2
 
 
 def test_metadata_long_break_clock_count_2(metadata_class):
-    """assigned long_break_clock_count value with non-integer less than 2"""
+    """Test long_break_clock_count <2 (float) round up to 2"""
     metadata_default = metadata_class(long_break_clock_count=1.5)
 
     assert metadata_default.long_break_clock_count == 2
 
 
 def test_metadata_long_break_clock_count_3(metadata_class):
-    """assigned long_break_clock_count value with integer larger than 2"""
+    """Test long_break_clock_count assigned interger stay the same if > 2"""
     metadata_default = metadata_class(long_break_clock_count=3)
 
     assert metadata_default.long_break_clock_count == 3
 
 
 def test_metadata_long_break_clock_count_4(metadata_class):
-    """assigned long_break_clock_count value with non-integer larger than 2
+    """Assigned long_break_clock_count value with float K+ 0.5 round down.
 
-    Besides, testing downward rounding
+
     """
     metadata_default = metadata_class(long_break_clock_count=4.4)
 
@@ -63,10 +63,11 @@ def test_metadata_long_break_clock_count_4(metadata_class):
 
 
 def test_metadata_long_break_clock_count_5(metadata_class):
-    """assigned long_break_clock_count value with non-integer larger than 2
+    """Assigned long_break_clock_count value with non-integer >0.5 round up.
 
-    Besides, testing upward rounding
+
     """
     metadata_default = metadata_class(long_break_clock_count=5.5)
 
     assert metadata_default.long_break_clock_count == 6
+
--- tests/test_timer.py
+++ tests/test_timer.py
@@ -12,7 +12,7 @@ def fake_timer():
     db_path_debug = utils.get_cache_filepath(None, debug=True)
     db_file_debug = f"{db_path_debug}/ctimer_debug_2021.db"
     db.create_connection(db_file_debug)
-    current_clock_details= db.Clock_details()
+    current_clock_details = db.Clock_details(db_file_debug)
 
     fake_model = CtimerClockModel(
         db_file_debug, current_clock_details, False, False, False, None
@@ -22,7 +22,19 @@ def fake_timer():
     return fake_timer
 
 
+def test_fake_timer_new_clock(fake_timer):
+    """
+    New launched clock status.
+    """
+    assert fake_timer.tm.fresh_new
+    assert not fake_timer.tm.clock_details.is_break
+    assert not fake_timer.tm.clock_details.is_complete
+
+
 def test_fake_timer_toggle_start_pause(fake_timer):
+    """
+    Toggle start pause button affects ticking status
+    """
     assert not fake_timer.tm.clock_ticking
 
     fake_timer.toggle_start_pause()
@@ -32,3 +44,38 @@ def test_fake_timer_toggle_start_pause(fake_timer):
     fake_timer.toggle_start_pause()
 
     assert not fake_timer.tm.clock_ticking
+
+
+def test_fake_timer_countdown(fake_timer):
+    """
+    A counting down ctimer would stay as not is_break after start / pause / start / pause cycle as long as
+    timer countdown hasn't reached.
+    """
+    assert not fake_timer.tm.clock_ticking
+
+    fake_timer.toggle_start_pause()
+    assert fake_timer.tm.clock_ticking
+    assert not fake_timer.tm.clock_details.is_break
+
+    fake_timer.toggle_start_pause()
+    assert not fake_timer.tm.clock_ticking
+    assert not fake_timer.tm.clock_details.is_break
+
+
+def test_fake_timer_terminate(fake_timer):
+    """
+    A counting down ctimer would stop ticking after terminate, and reset
+
+    1. The remaining time will become set time.
+    2. Start/Pause Button text show "Start"
+    3. is_break == False
+    """
+    assert not fake_timer.tm.clock_ticking
+
+    fake_timer.toggle_start_pause()
+    assert fake_timer.tm.clock_ticking
+    assert not fake_timer.tm.clock_details.is_break
+
+    fake_timer.terminate()
+    assert not fake_timer.tm.clock_ticking
+    assert not fake_timer.tm.clock_details.is_break
